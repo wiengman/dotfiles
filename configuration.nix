3fa99d52f8c4ab8bf6a9f08f9fe5db7054364ef5
William Engman <william.engman@hotmail.com>
2024-07-31 19:23:27 +0200

    add bluetooth

--- configuration.nix
+++ configuration.nix
@@ -122,6 +122,11 @@
    fonts.fontconfig.defaultFonts.monospace = [ "CascadiaCode" ];
 
 
+  
+  hardware.bluetooth.enable = true;
+  hardware.bluetooth.powerOnBoot = true;
+  services.blueman.enable = true;
+
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
   # programs.mtr.enable = true;
