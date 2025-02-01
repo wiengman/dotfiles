e8afe9871bce3a2841a1d13b25ec0df45a5db2af
William Engman <william.engman@hotmail.com>
2025-02-01 15:33:22 +0100

    sync

--- fish/config.fish
+++ fish/config.fish
@@ -3,9 +3,23 @@ if status is-interactive
 end
 
 
+# Alias
+alias flatseal="flatpak run com.github.tchx84.Flatseal"
+alias ll="ls -la"
+alias discord="flatpak run dev.vencord.Vesktop --disable-gpu"
+alias protontricks="flatpak run com.github.Matoking.protontricks"
+alias protontricks-launch="flatpak run --command=protontricks-launch com.github.Matoking.protontricks"
+alias spotify="flatpak run com.spotify.Client"
+alias steam="flatpak run com.valvesoftware.Steam"
+alias obs="flatpak run com.obsproject.Studio"
+alias monitor="flatpak run io.missioncenter.MissionCenter"
+
+
 # Add path
 set -U fish_user_paths ~/dev/eww/target/release/ ~/bin ~/.cargo/bin
 set -U EDITOR nvim
+
+# Remove header
 set fish_greeting
 
 starship init fish | source
